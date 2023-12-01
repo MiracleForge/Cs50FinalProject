6c5c8deb65a4b4504584636f38b103594cec355b
MiracleForge <paulomoreirarosado@hotmail.com>
2023-12-01 17:48:42 -0300

    First nav complete - Login/register off/on in toogler

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Adiciona um ouvinte de eventos para detectar quando o toggle é mostrado ou ocultado
+document.getElementById('main-nav').addEventListener('show.bs.collapse', function () {
+    // Verifica se o toogle está sendo mostrado
+    if (document.getElementById('main-nav').classList.contains('show')) {
+        // Esconde os elementos de Login e Register na primeira navegação
+        document.getElementById('login').classList.add('d-none');
+        document.getElementById('register').classList.add('d-none');
+
+        // Exibe os elementos de Login e Register na segunda navegação
+        document.getElementById('loginMobile').classList.remove('d-none');
+        document.getElementById('registerMobile').classList.remove('d-none');
+    }
+});
+
+document.getElementById('main-nav').addEventListener('hide.bs.collapse', function () {
+    // Verifica se o toogle está sendo escondido
+    if (!document.getElementById('main-nav').classList.contains('show')) {
+        // Exibe os elementos de Login e Register na primeira navegação
+        document.getElementById('login').classList.remove('d-none');
+        document.getElementById('register').classList.remove('d-none');
+
+        // Esconde os elementos de Login e Register na segunda navegação
+        document.getElementById('loginMobile').classList.add('d-none');
+        document.getElementById('registerMobile').classList.add('d-none');
+    }
+});
+
