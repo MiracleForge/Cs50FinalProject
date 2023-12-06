3f6f5b31776b0ab19e1fb6ec12b6bfecd0b1c676
MiracleForge <paulomoreirarosado@hotmail.com>
2023-12-06 12:25:22 -0300

    Final version of annnounce, error messages -done announce type -done, announce page -done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, get_flashed_messages
+from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -28,6 +28,17 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+@app.before_request
+def before_request():
+    g.display_username = None  
+
+    if "user_id" in session:
+        user_id = session["user_id"]
+        result = db.execute("SELECT username FROM autenticacao WHERE id = ?", (user_id,))
+        g.display_username = result[0]["username"] if result else None
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -40,6 +51,7 @@ def index():
 
     return render_template("layout.html")
 
+
 #Login routes
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -47,39 +59,37 @@ def login():
         return redirect("/")
     
     if request.method == 'GET':
-
         return render_template("login.html")
     
+    error_message = None
     username_input = request.form.get("username")
     password_input = request.form.get("password")
 
     if not username_input or not password_input:
-        flash("You must provide a username and password", 403)
-        return render_template("login.html")
-    
-    rows = db.execute(
-        "SELECT * FROM autenticacao WHERE username = ?", username_input
-        )
-    
-     # Ensure username exists and password is correct
-    if len(rows) != 1 or not check_password_hash(
-        rows[0]["password"], password_input
-    ):
-        return render_template("login.html")
-    
-    # Remember which user has logged in
-    session["user_id"] = rows[0]["id"]
-
-    # Redirect user to home page
-    return redirect("/")
+        error_message = "403 - You must provide a username and password"
+    else:
+        rows = db.execute("SELECT * FROM autenticacao WHERE username = ?", username_input)
         
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], password_input):
+            error_message = "403 - Authentication Failed: The provided username does not exist or the password is incorrect"
+        else:
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["id"]
+            # Redirect user to home page
+            return redirect("/")
+    
+    return render_template("login.html", error=error_message)
+
+        
+
+from flask import render_template
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-
     if "user_id" in session:
         return redirect("/")
-    
+
     if request.method == 'GET':
         return render_template("register.html")
 
@@ -90,51 +100,56 @@ def register():
     email_input = request.form.get("email")
     user_isvalid = True
 
-    # Validation queries
-    validation_username = db.execute("SELECT * FROM autenticacao WHERE username = ?", username_input)
-    validation_email = db.execute("SELECT * FROM autenticacao WHERE email = ?", email_input)
+    error_message = None
+    message = None
 
-    # Check if username or email is already in use
-    if len(validation_username) == 1:
-        flash("This username is already in use", "error")
-        user_isvalid = False
+    try:
+        # Validation queries
+        validation_username = db.execute("SELECT * FROM autenticacao WHERE username = ?", username_input)
+        validation_email = db.execute("SELECT * FROM autenticacao WHERE email = ?", email_input)
 
-    elif len(validation_email) == 1:
-        flash("This email is already in use", "error")
-        user_isvalid = False
+        # Check if username or email is already in use
+        if len(validation_username) == 1:
+            error_message = "403 - This username is already in use"
+            user_isvalid = False
 
-    # Check for empty fields
-    if not username_input or not password_input or not confirm_input or not email_input:
-        flash("Please fill in all the fields", "error")
-        user_isvalid = False
+        elif len(validation_email) == 1:
+            error_message = "403 - This email is already in use"
+            user_isvalid = False
 
+        # Check for empty fields
+        if not username_input or not password_input or not confirm_input or not email_input:
+            error_message = "403 - Please fill in all the fields"
+            user_isvalid = False
 
-    if password_input != confirm_input:
-        flash("Passwords must be identical", "error")
-        user_isvalid = False
+        if password_input != confirm_input:
+            error_message = "403 - Passwords must be identical"
+            user_isvalid = False
 
-
-    if not (
+        if not (
             re.search(r"[a-zA-Z]", password_input)
             and re.search(r"\d", password_input)
             and re.search(r"[!@#$%^&*(),.?\":{}|<>]", password_input)
         ):
-        flash("Password must contain at least one letter, one digit, and one special character", "error")
-        user_isvalid = False
-    
-    #Hashing password
-    if user_isvalid:
-        password_hashed = generate_password_hash(password_input)
+            error_message = "403 - Password must contain at least one letter, one digit, and one special character"
+            user_isvalid = False
 
-        # UPDATING DATABASE
-        db.execute("INSERT INTO autenticacao (username, password, email) VALUES (?, ?, ?)", username_input, password_hashed, email_input)
+        # Hashing password and adding to the database only if all validations pass
+        if user_isvalid:
+            password_hashed = generate_password_hash(password_input)
+            db.execute("INSERT INTO autenticacao (username, password, email) VALUES (?, ?, ?)", username_input, password_hashed, email_input)
 
-        # Registration successful
-        flash("Registration successful!", "success")
-        return redirect("/login")
-    else:
-        
-        return redirect("/register")
+            # Registration successful
+            message ="Registration successful"
+            return render_template("login.html", message= message )
+
+    except Exception as e:
+
+        error_message = "500 - Internal Server Error"
+
+    # Render the template com a mensagem de erro
+    return render_template("register.html", error=error_message)
+
 
 
 @app.route("/logout")
@@ -159,19 +174,25 @@ def myads():
             return render_template("login.html")
         
 #trying to work with git
-@app.route("/announce", methods = ["GET", "POST"])
+@app.route("/announce", methods=["GET", "POST"])
 @login_required
 def announce():
+    error_message = None
+
     if request.method == "GET":
         return render_template("announce.html")
-    
+
     if request.method == "POST":
         ad_type_return = request.form.get("ad_category")
 
-        if not ad_type_return:
-            return render_template("announce.html")
+        ad_types_allowed = ['houses_Ad', "ownedCars_Ad", "furniture_Ad", "tech_Ad", "musical_Ad", "toys_Ad", "pet_Ad", "office_Ad", "fashion_Ad", "games_Ad"]
+        
+        if ad_type_return not in ad_types_allowed:
+            error_message = "400 - Ad_type_notAllowed"
+        else:
+            return render_template("AdsCreate.html", ads_type=ad_type_return)
 
-        return render_template("AdsCreate.html", ads_type=ad_type_return)
+    return render_template("announce.html", error=error_message)
 
 
 
